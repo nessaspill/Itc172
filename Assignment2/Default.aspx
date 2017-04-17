1572a363d4542fba1ccae1fc4ca14d5a0747259c
nessaspill <vanessa.spillari@seattlecentral.edu>
2017-04-17 03:57:30 -0700

    Commit Assignment2

---
+++ Assignment2/Default.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Events Calendar</title>
+    <link href="Style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div class="wrap">
+        <h1 class="title">Book Readings</h1>
+        <h2>If you want to see all our events click in the dates</h2>
+        <asp:Calendar ID="EventCalendar" runat="server" OnDayRender="EventCalendar_DayRender" OnSelectionChanged="EventCalendar_SelectionChanged" BackColor="White" BorderColor="White" BorderWidth="1px" Font-Names="Verdana" Font-Size="9pt" ForeColor="Black" Height="190px" NextPrevFormat="FullMonth" Width="350px">
+            <DayHeaderStyle Font-Bold="True" Font-Size="8pt" />
+            <NextPrevStyle Font-Bold="True" Font-Size="8pt" ForeColor="#333333" VerticalAlign="Bottom" />
+            <OtherMonthDayStyle ForeColor="#999999" />
+            <SelectedDayStyle BackColor="#333399" ForeColor="White" />
+            <TitleStyle BackColor="White" BorderColor="Black" BorderWidth="4px" Font-Bold="True" Font-Size="12pt" ForeColor="#333399" />
+            <TodayDayStyle BackColor="#CCCCCC" />
+        </asp:Calendar>
+        <h2><asp:Label ID="LabelCalendar1" runat="server" Text=""></asp:Label></h2>
+        <p><asp:Label ID="LabelEvent" runat="server" Text=""></asp:Label></p>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Assignment2/Default.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    string[,] events = new string[6, 2];
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        events[0, 0] = "2017-04-20";
+        events[0, 1] = "The Grapes of Wrath";
+        events[1, 0] = "2017-04-21";
+        events[1, 1] = "The Martian Chronicles";
+        events[2, 0] = "2017-04-10";
+        events[2, 1] = "I Robot";
+        events[3, 0] = "2017-04-10";
+        events[3, 1] = "The Hobbit";
+        events[4, 0] = "2017-04-30";
+        events[4, 1] = "Something Wicked This Way Comes";
+        events[5, 0] = "2017-04-03";
+        events[5, 1] = "Something Wicked This Way Comes";
+
+    }
+
+    protected void EventCalendar_DayRender(object sender, DayRenderEventArgs e)
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            if (e.Day.Date == DateTime.Parse(events[i, 0]))
+            {
+                e.Cell.ForeColor = System.Drawing.Color.DarkRed;
+                e.Cell.Style.Add(HtmlTextWriterStyle.FontWeight, "bold");
+            }
+        }
+    }
+
+    protected void EventCalendar_SelectionChanged(object sender, EventArgs e)
+    {
+        LabelEvent.Text = "";
+        string eventDate;
+       // bool found = false;
+        eventDate = EventCalendar.SelectedDate.ToString("yyyy-MM-dd");
+        LabelCalendar1.Text = "Events in " + EventCalendar.SelectedDate.ToShortDateString();
+        for (int i = 0; i < 6; i++)
+        {
+            if (events[i, 0].Equals(eventDate))
+            {
+                LabelEvent.Text += events[i, 1] + "<br />";
+               // found = true;
+            }
+        }
+
+        //if(!found){}
+        if (LabelEvent.Text == "") {
+            LabelEvent.Text += "There are no events in this date.";
+        }
+    }
+}
+
